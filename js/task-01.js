6c27ddd78442ada55b1185bbc6247ee32ab4ada6
SergeZed <75592154+SergeZed@users.noreply.github.com>
2022-06-04 12:15:23 +0700

    Task 1. updated

--- js/task-01.js
+++ js/task-01.js
@@ -4,11 +4,10 @@ const navHeadEl = navEl.querySelectorAll("li.item");
 console.log("Number of categories:", navHeadEl.length);
 console.log("\n");
 
-const navCategory0El = navEl.querySelectorAll("h2");
-console.log("Category:", navCategory0El[0].textContent);
+navHeadEl.forEach((elem) => {
+  const navCategoryEl = elem.querySelectorAll("h2");
+  console.log("Category:", navCategoryEl[0].textContent);
 
-const navELements = navEl.querySelectorAll("ul");
-navELements.forEach((elem) => {
   const navElementCount = elem.querySelectorAll("li");
   console.log("Elements:", navElementCount.length);
   console.log("\n");
